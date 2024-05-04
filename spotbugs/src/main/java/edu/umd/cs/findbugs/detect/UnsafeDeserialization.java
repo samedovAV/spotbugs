0f2b4848151c5d68a2f26c584a0422a8b2667cbc
Aleksandr Samedov <emsmx4@inf.elte.hu>
2024-05-04 21:52:10 +0200

    SER06-J. Make defensive copies of private mutable components during deserialization

--- spotbugs/src/main/java/edu/umd/cs/findbugs/detect/UnsafeDeserialization.java
+++ spotbugs/src/main/java/edu/umd/cs/findbugs/detect/UnsafeDeserialization.java
@@ -30,14 +30,14 @@ import org.apache.bcel.classfile.Method;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class UnsafeDeserialization extends OpcodeStackDetector {
 
-    // Collections.copy(dest, src) has 2 arguments, we need src
-    public static final int STACK_ITEM_FOR_COPY = 1;
+    public static final int STACK_ITEM_FOR_COPY = 1; // Collections.copy(dest, src) has 2 arguments, we need src
+    public static final int STACK_DEPTH_FOR_SYSTEM_ARRAYCOPY = 3; // Minimum stack depth for System.arraycopy()
+    public static final int STACK_ITEM_FOR_SYSTEM_ARRAYCOPY = 2; // System.arraycopy() has 3 arguments, we need the second one
 
     private final BugReporter bugReporter;
 
@@ -48,28 +48,25 @@ public class UnsafeDeserialization extends OpcodeStackDetector {
     }
 
     private Method isReadObjectFoundAndGet(JavaClass obj) {
-        Optional<Method> readObjMethod = Arrays.stream(obj.getMethods())
-                .filter(i -> i.getName().equals("readObject")
-                        && "(Ljava/io/ObjectInputStream;)V".equals(i.getSignature())
-                        && i.isPrivate())
-                .findFirst();
-
-        return readObjMethod.orElse(null);
+        return Arrays.stream(obj.getMethods())
+                .filter(this::isReadObject)
+                .findFirst()
+                .orElse(null);
     }
 
-    private boolean isReadObjectFound() {
-        return "readObject".equals(getMethodName())
-                && "(Ljava/io/ObjectInputStream;)V".equals(getMethodSig())
-                && getMethod().isPrivate();
+    private boolean isReadObject(Method method) {
+        return "readObject".equals(method.getName())
+                && "(Ljava/io/ObjectInputStream;)V".equals(method.getSignature())
+                && method.isPrivate();
     }
 
-    private boolean isSerializable() {
-        return Subtypes2.instanceOf(getClassContext().getJavaClass(), "java.io.Serializable");
+    private boolean isSerializable(JavaClass javaClass) {
+        return Subtypes2.instanceOf(javaClass, "java.io.Serializable");
     }
 
     @Override
     public void visit(Field obj) {
-        if (isSerializable()) {
+        if (isSerializable(getClassContext().getJavaClass())) {
             XField xField = getXField();
             if (!xField.isStatic()
                     && (xField.isPrivate() || xField.isFinal())
@@ -85,7 +82,8 @@ public class UnsafeDeserialization extends OpcodeStackDetector {
 
     @Override
     public void visitAfter(JavaClass obj) {
-        if (!mutableFields.isEmpty() && isReadObjectFoundAndGet(obj) != null) {
+        Method readObjectFoundAndGet = isReadObjectFoundAndGet(obj);
+        if (!mutableFields.isEmpty() && readObjectFoundAndGet != null) {
             String allFields = mutableFields.stream()
                     .map(XField::getName)
                     .sorted()
@@ -93,13 +91,13 @@ public class UnsafeDeserialization extends OpcodeStackDetector {
             bugReporter.reportBug(new BugInstance("UD_UNSAFE_DESERIALIZATION_DEFENSIVE_COPIES", NORMAL_PRIORITY)
                     .addClass(obj)
                     .addString(allFields)
-                    .addMethod(obj, isReadObjectFoundAndGet(obj)));
+                    .addMethod(obj, readObjectFoundAndGet));
         }
     }
 
     @Override
     public void sawOpcode(int seen) {
-        if (isSerializable() && isReadObjectFound()) {
+        if (isSerializable(getClassContext().getJavaClass()) && isReadObject(getMethod())) {
             // There are two ways to make a defensive copy:
             // 1. Assignment: PUTFILED, PUTSTATIC
             // 2. Method call: INVOKESTATIC, INVOKEVIRTUAL, INVOKESPECIAL, INVOKEINTERFACE
@@ -114,14 +112,12 @@ public class UnsafeDeserialization extends OpcodeStackDetector {
                 if ("copy".equals(methodName) && "java/util/Collections".equals(getClassConstantOperand())
                         && stackDepth > STACK_ITEM_FOR_COPY) { // check method before the stack
                     XField field = stack.getStackItem(STACK_ITEM_FOR_COPY).getXField();
-                    if (mutableFields.contains(stack.getStackItem(STACK_ITEM_FOR_COPY).getXField())) {
-                        mutableFields.remove(field);
-                    }
+                    mutableFields.remove(field);
                 }
                 // System.arraycopy()
                 if ("arraycopy".equals(methodName) && "java/lang/System".equals(getClassConstantOperand())
-                        && stackDepth >= 3) {
-                    XField field = stack.getStackItem(2).getXField();
+                        && stackDepth >= STACK_DEPTH_FOR_SYSTEM_ARRAYCOPY) {
+                    XField field = stack.getStackItem(STACK_ITEM_FOR_SYSTEM_ARRAYCOPY).getXField();
                     mutableFields.remove(field);
                 }
             }
